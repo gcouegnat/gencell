217e7ae60a55c79e295a9975fb60fb69d18c2572
Guillaume Couégnat <guillaume.couegnat@gmail.com>
2017-11-16 12:20:51 +0100

    Add some tests

---
+++ tests/test_pmubc.py
@@ -0,0 +1,13 @@
+from gencell.meshutils import read_mesh
+from gencell.feutils import isotropic_stiffness, isotropic_thermal_expansion
+from gencell.fetools import compute_effective_properties_PMUBC
+
+m = read_mesh('meshes/cube.mesh')
+
+mat = [(isotropic_stiffness(E=100e3, nu=0.2),
+        isotropic_thermal_expansion(alpha=1.0e-6)),
+       ]
+
+Ceff, aeff = compute_effective_properties_PMUBC(m, mat)
+print Ceff
+print aeff
